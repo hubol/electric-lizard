619b6913f8999fc4df136956e95a4eb8d0b2dd3a
Hubol <hubol.gordon@gmail.com>
2021-12-24 00:46:23 -0600

    Save window settings for convenience ^_^

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const electron = require("electron");
+const Store = require("electron-store");
 const app = electron.app;
 
 const BrowserWindow = electron.BrowserWindow;
+
+const store = new Store();
 
 const path = require("path");
 
 let window;
 
-function createWindow() {
-    window = new BrowserWindow({ autoHideMenuBar: true, maximizable: false });
+function getStorableWindowProps(window) {
+    const [width, height] = window.getSize();
+    return { width, height, fullscreen: window.isFullScreen() };
+}
 
+function createWindow() {
+    const windowProps = store.get('window');
+    window = new BrowserWindow({ autoHideMenuBar: true, maximizable: false, ...windowProps });
     window.loadURL(`file://${path.join(__dirname, "app/index.html")}`);
-    window.on("closed", () => (window = null));
+
+    const saveWindowProps = () => store.set('window', getStorableWindowProps(window));
+
+    window.on("resize", saveWindowProps);
+    window.on("enter-full-screen", saveWindowProps);
+    window.on("leave-full-screen", saveWindowProps);
+    window.on("closed", () => window = null);
 }
 
 app.on("ready", createWindow);
