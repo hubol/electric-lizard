7759f70a0193eb1897f625a630ab3832efce62a2
Hubol <hubol.gordon@gmail.com>
2026-08-01 12:03:08 -0500

    Special ICM branch that starts in fullscreen, maybe other features later

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
 const electron = require("electron");
-const Store = require("electron-store");
 const app = electron.app;
 
 const BrowserWindow = electron.BrowserWindow;
-
-const store = new Store();
 
 const path = require("path");
 
 let window;
 
-function getStorableWindowProps(window) {
-    const [width, height] = window.getSize();
-    return { width, height, fullscreen: window.isFullScreen() || undefined };
-}
-
 function createWindow() {
-    const windowProps = store.get('window');
-    window = new BrowserWindow({ autoHideMenuBar: true, maximizable: false, ...windowProps });
+    window = new BrowserWindow({ autoHideMenuBar: true, maximizable: false, fullscreen: true });
     window.loadURL(`file://${path.join(__dirname, "app/index.html")}`);
 
-    const saveWindowProps = () => store.set('window', getStorableWindowProps(window));
-
-    window.on("resize", saveWindowProps);
-    window.on("enter-full-screen", saveWindowProps);
-    window.on("leave-full-screen", saveWindowProps);
     window.on("closed", () => window = null);
 }
 
